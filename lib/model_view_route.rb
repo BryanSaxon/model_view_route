f6941e5c7cfb632996f035b2f40ffbde3e9e2096
Bryan Saxon <emailBryanSaxon@gmail.com>
2016-05-16 19:44:55 -0500

    initial commit.

---
+++ lib/model_view_route.rb
@@ -0,0 +1,5 @@
+require "model_view_route/version"
+
+module ModelViewRoute
+  # Your code goes here...
+end
---
+++ test/model_view_route_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ModelViewRouteTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ModelViewRoute::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
